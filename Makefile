ce1efc10904c7550dd3c34fb56d1859683aeedc3
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-03-13 10:00:10 +0200

    add readAll lambda

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ build:
 	dep ensure
 	env GOOS=linux go build -ldflags="-s -w" -o bin/read read/main.go
 	env GOOS=linux go build -ldflags="-s -w" -o bin/create create/main.go
+	env GOOS=linux go build -ldflags="-s -w" -o bin/readAll readAll/main.go
---
+++ readAll/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"fmt"
+	"os"
+	"context"
+)
+
+type GetItemEvent struct {
+	Id string`json:"id"`
+}
+
+type MyDataItem struct {
+	Id string`json:"id"`
+	Data string`json:"data"`
+}
+
+func withDynamoSession() (*dynamodb.DynamoDB){
+	mySession, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1")},
+	)
+	if err != nil {
+		fmt.Println("Got error calling createSession:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return dynamodb.New(mySession)
+}
+
+func Handler(ctx context.Context, event GetItemEvent) ([]MyDataItem, error) {
+
+	var itemsSlice []MyDataItem
+
+	err := withDynamoSession().ScanPages(&dynamodb.ScanInput{
+		TableName: aws.String("Movies"),
+	}, func(page *dynamodb.ScanOutput, last bool) bool {
+		recs := []MyDataItem{}
+
+		err := dynamodbattribute.UnmarshalListOfMaps(page.Items, &recs)
+		if err != nil {
+			panic(fmt.Sprintf("failed to unmarshal Dynamodb Scan Items, %v", err))
+		}
+
+		itemsSlice = append(itemsSlice, recs...)
+		return true // keep paging
+	})
+
+	if err != nil {
+		fmt.Println("Got error calling Scan:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return itemsSlice, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
